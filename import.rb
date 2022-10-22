9d0590286a79174ad638fdd7c0f3bbbdfbda5e08
SHshzik <andrycalnicow@gmail.com>
2022-10-23 03:04:36 +0400

    Sync ingredients

--- import.rb
+++ import.rb
@@ -1,22 +1,27 @@
-# require 'sqlite3'
-# require 'csv'
-#
-# db = SQLite3::Database.new 'potionomics.db'
-#
-# table = CSV.parse(File.read('1.csv'), headers: true)
-#
-# table.each do |row|
-#   db.execute(
-#     "insert into ingredients values ( ?, ?, ?, ?, ?, ?, ?, ? )",
-#     [
-#       row['Ingredients'],
-#       row['A'].to_i,
-#       row['B'].to_i,
-#       row['C'].to_i,
-#       row['D'].to_i,
-#       row['E'].to_i,
-#       row['Price (Quinn)'].to_i,
-#       0
-#     ]
-#   )
-# end
+# frozen_string_literal: true
+
+require 'csv'
+require './models/ingredient'
+
+table = CSV.parse(File.read('1.csv'), headers: true)
+
+table.each do |row|
+  p row['Ingredients']
+  p row['A']
+  p row['B']
+  p row['C']
+  p row['D']
+  p row['E']
+  p row['Price (Quinn)']
+  Ingredient.find_or_create(
+    name: row['Ingredients'],
+    a: row['A'].to_i,
+    b: row['B'].to_i,
+    c: row['C'].to_i,
+    d: row['D'].to_i,
+    e: row['E'].to_i,
+    price: row['Price (Quinn)'].to_i
+  )
+end
+
+p 'WARNING' if table.count != Ingredient.count

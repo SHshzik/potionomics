43ded983d7f9fabbb6b41ebd22ced97ef1064fff
SHshzik <andrycalnicow@gmail.com>
2022-10-23 01:23:02 +0400

    Add generate brew

---
+++ generate_brew.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'highline'
+require './potion'
+require './ingredient'
+require './brew'
+
+cli = HighLine.new
+
+potions = Potion.all
+
+# select potion for draw
+selected_potion = nil
+cli.choose do |menu|
+  menu.prompt = "Выбери зелье: "
+  potions.each do |potion|
+    menu.choice(potion.name) { selected_potion = potion }
+  end
+end
+
+ingredients = Ingredient.all
+
+capacity = cli.ask("Вместимость ингредиентов: ", Integer)
+
+ingredients.repeated_combination(capacity)
+           .select { Brew.valid?(_1, selected_potion.receipt) }
+           .each do |ingredients|
+             Brew.create do |brew|
+               brew.potion = selected_potion
+               magimin = ingredients.map(&:magimin).transpose.map(&:sum)
+               brew.magimin = magimin.to_s
+               brew.total_magimin = magimin.sum
+               brew.total_price = ingredients.sum(&:price)
+               brew.ingredients_count = capacity
+               brew.ingredient_ids = ingredients.sort_by(&:name).map(&:id).to_s
+             end
+           end
